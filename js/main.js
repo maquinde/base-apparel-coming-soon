9592c85c73f62d7f0d04b868a3a2315d278c8fe1
Mike <maquinde@outlook.com>
2020-05-07 21:32:36 -0700

    completed email validation in form and error display

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// 1:28
+const form = document.querySelector("form");
+const small = document.querySelector(".error-small");
+const email = document.querySelector(".email");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const emailVal = email.value;
+
+    if(!validateEmail(emailVal)){
+        form.classList.add("error");
+        small.style.display = "block";
+    } else {
+        form.classList.remove("error");
+        small.style.display = "none";
+    }
+
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+});
